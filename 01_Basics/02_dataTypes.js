6ff0ae06ac260a33abe41e5ce9de7f35ae13b45c
henilvaland <henilvaland@gmail.com>
2024-09-29 18:31:32 +0530

    dataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as a newer version
+
+let number = 23;
+const hugeHex = BigInt("0x1fffffffffffff");
+let greet = "Hello!";
+let isUserLoggedIn = true;
+let User = null; //standalone value
+let myName; // undefined 
+// symbol => unique
+// object
+
+console.log(typeof number);
+console.log(typeof hugeHex);
+console.log(typeof greet);
+console.log(typeof isUserLoggedIn);
+console.log(typeof User);
+console.log(typeof myName);
+
+
+
+

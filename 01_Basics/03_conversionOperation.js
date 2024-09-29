c5db006d9e7758b6054dde0fcdccfc8e23543837
henilvaland <henilvaland@gmail.com>
2024-09-29 20:02:21 +0530

    converstion operations

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,43 @@
+let score = "henil";
+//* console.log(typeof score);
+
+let valueInNumber = Number(score);
+//* console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN => typeof Nan = Number
+//  true => 1; false => 0
+
+let isLoggedIn = "henil";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+//* console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false (given empty string then false)
+// "henil" => true (given value in string then true)
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+//* console.log(stringNumber);
+//* console.log(typeof stringNumber);
+
+//************* Operations *****************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello ";
+let str2 = "henil";
+
+let str3 = str1 + str2;
+console.log(str3);

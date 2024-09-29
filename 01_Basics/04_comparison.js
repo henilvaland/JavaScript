00db638101d46089001115b3d71f27d4a7c4415b
henilvaland <henilvaland@gmail.com>
2024-09-29 20:14:44 +0530

    comparisons

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,22 @@
+//* console.log(2 > 1);
+//* console.log(2 >= 1);
+//* console.log(2 < 1);
+//* console.log(2 == 1);
+//* console.log(2 != 1);
+
+//string gets converted to number
+//* console.log("2" > 1);
+//* console.log("02" > 1);
+
+//* console.log(null > 0);
+//* console.log(null == 0);
+// here before comparion check the null gets converted to number
+//* console.log(null >= 0);
+
+// in all cases comparison with undefined will stay as undefined
+//* console.log(undefined == 0);
+//* console.log(undefined > 0);
+//* console.log(undefined < 0);
+
+// === strinct check including type and value
+//* console.log("2" === 2);
